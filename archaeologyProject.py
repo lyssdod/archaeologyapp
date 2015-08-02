c29fa05422b5b55e5bcda54bf045187adc9b9ef1
biletboh <biletskyboh@gmail.com>
2015-08-02 16:15:51 +0000

    commit before fixing the bug with delete site and edit

--- archaeologyProject.py
+++ archaeologyProject.py
@@ -18,56 +18,56 @@ class Site(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement = True)
     name = db.Column(db.String(250))
     toponim = db.Column(db.String(250))
-#    type_of_site= db.Column(db.String(250), nullable=False)
-#    oblast = db.Column(db.String(250), nullable=False)
-#    rajon = db.Column(db.String(250), nullable=False)
-#    punkt = db.Column(db.String(250), nullable=False)
-#    pryvjazka = db.Column(db.String(250), nullable=False)
-#    kultnal = db.Column(db.String(250), nullable=False)
-#    localgr = db.Column(db.String(250), nullable=False)
-#    chron = db.Column(db.String(250), nullable=False)
-#    nadijnist = db.Column(db.String(250), nullable=False)
-#    rozkop = db.Column(db.String(1000), nullable=False)
-#    zvit = db.Column(db.String(1000), nullable=False)
-#    publicacii = db.Column(db.String(1500), nullable=False)
-#    kartograph = db.Column(db.String(20), nullable=False)
-#    coord = db.Column(db.String(50), nullable=False)
-#    tochkart = db.Column(db.String(20), nullable=False)
-#    basejn = db.Column(db.String(50), nullable=False)
-#    toppotype = db.Column(db.String(30), nullable=False)
-#    geomorform = db.Column(db.String(250), nullable=False)
-#    vysotnadrm = db.Column(db.String(250), nullable=False)
-#    ploshch = db.Column(db.String(50), nullable=False)
-#    dovz = db.Column(db.String(50), nullable=False)
-#    shyr = db.Column(db.String(50), nullable=False)
-#    prymitky = db.Column(db.String(3000))
+    type_of_site= db.Column(db.String(250), nullable=False)
+    oblast = db.Column(db.String(250), nullable=False)
+    rajon = db.Column(db.String(250), nullable=False)
+    punkt = db.Column(db.String(250), nullable=False)
+    pryvjazka = db.Column(db.String(250), nullable=False)
+    kultnal = db.Column(db.String(250), nullable=False)
+    localgr = db.Column(db.String(250), nullable=False)
+    chron = db.Column(db.String(250), nullable=False)
+    nadijnist = db.Column(db.String(250), nullable=False)
+    rozkop = db.Column(db.String(1000), nullable=False)
+    zvit = db.Column(db.String(1000), nullable=False)
+    publicacii = db.Column(db.String(1500), nullable=False)
+    kartograph = db.Column(db.String(20), nullable=False)
+    coord = db.Column(db.String(50), nullable=False)
+    tochkart = db.Column(db.String(20), nullable=False)
+    basejn = db.Column(db.String(50), nullable=False)
+    toppotype = db.Column(db.String(30), nullable=False)
+    geomorform = db.Column(db.String(250), nullable=False)
+    vysotnadrm = db.Column(db.String(250), nullable=False)
+    ploshch = db.Column(db.String(50), nullable=False)
+    dovz = db.Column(db.String(50), nullable=False)
+    shyr = db.Column(db.String(50), nullable=False)
+    prymitky = db.Column(db.String(3000))
 #
     def __init__(self, name, toponim): #, type_of_site, oblast, rajon): #, punkt, pryvjazka, prymitky, kultnal, localgr, chron, nadijnist, rozkop, zvit, publicacii, kartograph, coord, tochkart, basejn, toppotype, geomorform, vysotnadrm, ploshch, dovz, shyr): 
         self.name = name
         self.toponim = toponim
-#        self.type_of_site = type_of_site
-#        self.oblast = oblast
-#        self.rajon = rajon
-#        self.punkt = punkt
-#        self.prymitky = prymitky
-#        self.kultnal = kultnal
-#        self.localgr = localgr
-#        self.chron = chron
-#        self.nadijnist = nadijnist
-#        self.rozkop = rozkop
-#        self.zvit = zvit
-#        self.publicacii = publicacii
-#        self.kartograph = kartograph
-#        self.coord = coord
-#        self.tochkart = tochkart
-#        self.basejn = basejn
-#        self.toppotype = toppotype
-#        self.geomorform = geomorform
-#        self.vysotnadrm = vysotnadrm
-##        self.ploshch = ploshch
-#        self.dovz = dovz
-#        self.shyr= shyr
-#
+        self.type_of_site = type_of_site
+        self.oblast = oblast
+        self.rajon = rajon
+        self.punkt = punkt
+        self.prymitky = prymitky
+        self.kultnal = kultnal
+        self.localgr = localgr
+        self.chron = chron
+        self.nadijnist = nadijnist
+        self.rozkop = rozkop
+        self.zvit = zvit
+        self.publicacii = publicacii
+        self.kartograph = kartograph
+        self.coord = coord
+        self.tochkart = tochkart
+        self.basejn = basejn
+        self.toppotype = toppotype
+        self.geomorform = geomorform
+        self.vysotnadrm = vysotnadrm
+        self.ploshch = ploshch
+        self.dovz = dovz
+        self.shyr= shyr
+
 #    @property #i might have to change this to __init__
 #    def serialize(self):
 #        """Return object data in easily serializeable format"""
