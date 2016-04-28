48ddfb100a9b111914a6e5f6d3f0b225f5606a54
lyssdod <lyssdod@gmail.com>
2016-04-28 06:30:10 +0300

    Revert "Update venv self references"
    This reverts commit 53dec8a1de35534a4db20e22214c24b76da41212.

--- djvenv/bin/activate.fish
+++ djvenv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelavent variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/root/djvenv"
+set -gx VIRTUAL_ENV "/root/archaeologyapp/djvenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
