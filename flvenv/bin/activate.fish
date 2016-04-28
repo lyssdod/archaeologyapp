53dec8a1de35534a4db20e22214c24b76da41212
lyssdod <lyssdod@gmail.com>
2016-04-28 06:10:16 +0300

    Update venv self references

--- flvenv/bin/activate.fish
+++ flvenv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/root/sheltered-tundra-8059/venv"
+set -gx VIRTUAL_ENV "/root/flvenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
