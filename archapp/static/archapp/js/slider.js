cecfe2d79f8a6c6e156597c95dd8d6542e79751e
lyssdod <lyssdod@gmail.com>
2016-05-22 22:49:11 +0300

    Fixed saving

--- archapp/static/archapp/js/slider.js
+++ archapp/static/archapp/js/slider.js
@@ -6,11 +6,13 @@
         {
             var l = $("<span id = 'slider_label'>aewoeirn908</span>")
             var z = $("<div id = 'slider'></div>");
+            var q = function(a, b) {
+                $('#id_datingfrom').val(a);
+                $('#id_datingto').val(b); }
             var x = function(e, ui) {
                 var r1 = d.find('option').eq(ui.values[0] - 1);
                 var r2 = d.find('option').eq(ui.values[1] - 1);
-                $('#id_datingfrom').val(r1[0].value);
-                $('#id_datingto').val(r2[0].value);
+                q(r1[0].value, r2[0].value);
                 l.html(r1.text() + ' - ' + r2.text()); }
             var s = z.slider({
                 min: 1,
@@ -22,6 +24,7 @@
                 if ($(this).is(':checked'))
                 {
                     z.slider('option', 'disabled', true);
+                    q(-1, -1);
                     l.html('---');
                 }
                 else
