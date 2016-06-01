0b4167ce5df6dc05effa70532b6b282c8c5af623
lyssdod <lyssdod@gmail.com>
2016-06-01 03:04:04 +0300

    Added parent property lookup (case-sensitive)

--- archapp/templatetags/lookup.py
+++ archapp/templatetags/lookup.py
@@ -1,10 +1,22 @@
 from django import template
+from archapp.models import Filter
 from django.utils.translation import ugettext as _
 
 register = template.Library()
 
 @register.filter
-def lookup(obj, name):
+def lookup(obj, args):
+    z = None
+
+    names = [arg.strip() for arg in args.split(',')]
+
     for x in obj:
-        if x.instance.name.lower() == name.lower():
-            return _(str(x))
+        if x.instance.name.lower() == names[0].lower():
+            z = x
+
+            if len(names) > 1:
+                # TODO: optimize
+                p = Filter.objects.filter(name = names[1]).get()
+                z = p.subfilters.filter(pk = x.integer).get()
+
+            return _(str(z))
