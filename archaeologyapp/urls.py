b0e292fb3278cb30a95d7cf154524e3bd3be04c6
lyssdod <lyssdod@gmail.com>
2016-06-03 01:54:24 +0300

    Added language selector

--- archaeologyapp/urls.py
+++ archaeologyapp/urls.py
@@ -17,7 +17,9 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = i18n_patterns(
+urlpatterns = [
+	url(r'^i18n/', include('django.conf.urls.i18n')),
+] + i18n_patterns(
     url(r'^archapp/', include('archapp.urls')),
     url(r'^admin/', admin.site.urls),
 )
