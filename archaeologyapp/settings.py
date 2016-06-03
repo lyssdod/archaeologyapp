762812b8b7e112b627ad6516a628a1e74ae89492
lyssdod <lyssdod@gmail.com>
2016-06-03 14:21:01 +0300

    Try to specify full path for settings

--- archaeologyapp/settings.py
+++ archaeologyapp/settings.py
@@ -13,14 +13,14 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 import configparser
 
-conf = configparser.ConfigParser()
-conf.read('settings.ini')
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
+
+conf = configparser.ConfigParser()
+conf.read(os.path.join(BASE_DIR, 'settings.ini'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = conf.get('archapp', 'secret', fallback = 'mldcn%7k0&#5fesf6wwensamw5*h^)_)_lhvj3*3&3rne!m79d')
