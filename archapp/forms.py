7d490e38e6ed225ea6a7ac5e637f44efb52ac5ed
lyssdod <lyssdod@gmail.com>
2016-07-04 19:19:39 +0300

    initial commented out

--- archapp/forms.py
+++ archapp/forms.py
@@ -31,7 +31,7 @@ class FilterForm(betterforms.BetterForm):
             if subs.count():
                 args['widget']  = forms.Select()
                 args['choices'] = [(s.id, _(s.name)) for s in subs]
-                args['initial'] = self.selected[flt] if self.selected is not None else subs[0].id
+                #args['initial'] = self.selected[flt] if self.selected is not None else subs[0].id
 
                 field = forms.ChoiceField()
 
