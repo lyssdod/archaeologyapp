629b52e1031e83c1ed5ffa11675c113cd298ee18
lyssdod <lyssdod@gmail.com>
2016-07-05 15:27:02 +0300

    small fixes

--- archapp/templatetags/choices.py
+++ archapp/templatetags/choices.py
@@ -6,6 +6,7 @@ register = template.Library()
 @register.filter
 def default_choice(field, choice):
     if choice:
+        choice_id = 0
         for e in field.field.choices:
             if e[1] == choice:
                 choice_id = e[0]
