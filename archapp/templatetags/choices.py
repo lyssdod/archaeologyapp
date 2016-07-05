46a2ef532a77fd92e677b4464e305fd07ac4848a
biletboh <biletskyboh@gmail.com>
2016-07-05 15:18:03 +0300

    fix choices

--- archapp/templatetags/choices.py
+++ archapp/templatetags/choices.py
@@ -5,9 +5,10 @@ register = template.Library()
 
 @register.filter
 def default_choice(field, choice):
-    for e in field.field.choices:
-        if e[1] == choice:
-            choice_id = e[0]
+    if choice:
+        for e in field.field.choices:
+            if e[1] == choice:
+                choice_id = e[0]
 
-    field.field.initial = choice_id
+        field.field.initial = choice_id
     return field
